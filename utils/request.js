101c1f0a06ec387274509520612d7251ff23f33d
ljswhhhhhh <11336706+ljswhhhhhh@user.noreply.gitee.com>
2022-09-26 21:20:27 +0800

    Initial Commit

---
+++ utils/request.js
@@ -0,0 +1,25 @@
+import config from '../utils/config'
+
+
+export default (url,data={},method='GET')=> {
+    // 返回promise实例
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url:config.host+url,
+            data,
+            method,
+            header:{
+              cookie:wx.getStorageSync('cookies')?wx.getStorageSync('cookies').toString():''
+            },
+            success:(res)=>{
+            //   console.log('成功:',res);
+              resolve(res.data)
+            },
+            fail:(err)=>{
+            //   console.log('失败:',err);
+              reject(err)
+            }
+        })
+    })
+    
+}

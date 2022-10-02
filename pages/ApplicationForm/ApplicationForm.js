d0df40b14ee6d6ff5e7a52cc2f1cf7a08a5f63ab
ljswhhhhhh <11336706+ljswhhhhhh@user.noreply.gitee.com>
2022-10-02 19:07:57 +0800

    前端验证

--- pages/ApplicationForm/ApplicationForm.js
+++ pages/ApplicationForm/ApplicationForm.js
@@ -1,11 +1,19 @@
 // pages/ApplicationForm/ApplicationForm.js
+import request from '../../utils/request'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    timeId: -1,
+    direction:'',
+    zhuanYe:'',
+    name:'',
+    studentNumber:'',
+    phone:'',
+    selfComment:''
   },
 
   /**
@@ -64,5 +72,49 @@ Page({
 
   },
 
-  
+
+
+  handleInput(e) {
+    clearTimeout(this.timeId)
+    this.timeId = setTimeout(() => {
+      let type=e.currentTarget.dataset.type
+      
+      this.setData({
+        [type]:e.detail.value
+      })
+    }, 300)
+  },
+
+  // 前端验证（验证手机格式之类的）
+  webVerify() {
+    let {direction,zhuanYe,name,studentNumber,phone,selfComment}=this.data;
+    console.log(direction.toUpperCase().indexOf('UI'));
+    // 首先保证全部填写
+    if (!direction || !zhuanYe || !name || !studentNumber || !phone || !selfComment) {
+      wx.showToast({
+        title: '好像有表格没填完哟',
+        icon:'none'
+      })
+      return
+    }
+
+    if (direction.indexOf('前端')==-1 && direction.indexOf('后端')==-1 && direction.indexOf('后台')==-1 && direction.toUpperCase().indexOf('UI')==-1 && direction.indexOf('运营')==-1) {
+      wx.showToast({
+        title: '好像没有这个方向哟',
+        icon:'none'
+      })
+      return
+    }
+
+    let phoneReg=/^1(3|4|5|6|7|8|9)\d{9}$/;
+    if (!phoneReg.test(phone)) {
+      wx.showToast({
+        title: '手机号格式错误',
+        icon:'none'
+      })
+      return; 
+    }
+    
+    // 给服务器发送请求
+  }
 })
